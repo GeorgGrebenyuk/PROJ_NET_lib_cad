c1912128506250db12186f4741c10dbd2840d070
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-09 14:31:22 +0300

    Upload dynamo package library

---
+++ dyn_proj_library/LibraryImport.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using dr = Autodesk.DesignScript.Runtime;
+
+namespace dyn_proj_library
+{
+    /// <summary>
+    /// Sctruct only for exporting data from extern cpp-function
+    /// </summary>
+    [dr.IsVisibleInDynamoLibrary(false)]
+    public struct point
+    {
+        /// <summary>
+        /// X, meters = latitude in degrees for geodetic coordinates
+        /// </summary>
+        public double x;
+        /// <summary>
+        /// Y, meters = longitude in degrees for geodetic coordinates
+        /// </summary>
+        public double y;
+        /// <summary>
+        /// Z, meters - altitude
+        /// </summary>
+        public double z;
+        public point(double x_input, double y_input, double z_input)
+        {
+            this.x = x_input;
+            this.y = y_input;
+            this.z = z_input;
+        }
+    }
+    [dr.IsVisibleInDynamoLibrary(false)]
+    public static class LibraryImport
+    {
+        public static ILibraryImport Select()
+        {
+            if (IntPtr.Size != 4) // 32-bit application
+            {
+                return new LibraryImport_x64();
+            }
+            else // 64-bit application
+            {
+                return null;
+            }
+        }
+    }
+    [dr.IsVisibleInDynamoLibrary(false)]
+    public interface ILibraryImport
+    {
+        point crs2crs_tranform(int source_cs, int target_cs, 
+            double source_x, double source_y, double source_z);
+
+    }
+    [dr.IsVisibleInDynamoLibrary(false)]
+    internal class LibraryImport_x64 : ILibraryImport
+    {
+        [DllImport("proj_functions_x64", CallingConvention = CallingConvention.StdCall, ExactSpelling = false, EntryPoint = "crs2crs_tranform")]
+        private static extern point crs2crs_tranform_internal(int source_cs, int target_cs, 
+            double source_x, double source_y, double source_z);
+
+        public point crs2crs_tranform(int source_cs, int target_cs, 
+            double source_x, double source_y, double source_z)
+        {
+            return crs2crs_tranform_internal(source_cs, target_cs, source_x, source_y, source_z);
+        }
+    }
+}
---
+++ dyn_proj_library/Reprojecting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dr = Autodesk.DesignScript.Runtime;
+using dg = Autodesk.DesignScript.Geometry;
+namespace dyn_proj_library
+{
+    /// <summary>
+    /// Class for data reproject
+    /// </summary>
+    public class Reprojecting
+    {
+        public point recalced;
+        /// <summary>
+        /// Reproject data (point) by codes of start and target coordinate system (EPSG code)
+        /// and coordinates of point
+        /// </summary>
+        /// <param name="cs_source_code">integer value of EPSG code for source coordinate system</param>
+        /// <param name="cs_target_code">integer value of EPSG code for target coordinate system</param>
+        /// <param name="coord_x">double-value of X-coordinate (too latitude)</param>
+        /// <param name="coord_y">double-value of Y-coordinate (too longitude)</param>
+        /// <param name="coord_z">double-value of Z-coordinate (too altitude)</param>
+        public Reprojecting (int cs_source_code, int cs_target_code, double coord_x, double coord_y, double coord_z)
+        {
+            ILibraryImport lib_wrapper = LibraryImport.Select();
+            this.recalced = lib_wrapper.crs2crs_tranform(cs_source_code, cs_target_code, coord_x, coord_y, coord_z);
+        }
+        /// <summary>
+        /// Getting reproject point as three coordinates (double-values)
+        /// </summary>
+        /// <returns></returns>
+        [dr.MultiReturn(new[] {"X","Y","Z"})]
+        public Dictionary<string,double> get_XYZ()
+        {
+            return new Dictionary<string, double>()
+            {
+                {"X",this.recalced.x },
+                {"Y",this.recalced.y },
+                {"Z",this.recalced.z }
+            };
+        }
+        /// <summary>
+        /// Getting reproject point as Dynamo geometry (Point)
+        /// </summary>
+        /// <returns></returns>
+        public dg.Point get_DynamoPoint()
+        {
+            return dg.Point.ByCoordinates(this.recalced.x, this.recalced.y, this.recalced.z);
+        }
+    }
+}
