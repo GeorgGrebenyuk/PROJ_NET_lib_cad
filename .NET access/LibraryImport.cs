e9ea87c0870ef716cc5026089aa763066f00bd2e
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-08-06 12:51:03 +0300

    Update from dyn-package experience

--- .NET access/LibraryImport.cs
+++ .NET access/LibraryImport.cs
@@ -27,39 +27,45 @@ namespace test_net
             this.z = z_input;
         }
     }
-    public static class LibraryImport
+    public class LibraryImport : IDisposable
     {
-        
-        public static ILibraryImport Select()
-        {
-            if (IntPtr.Size != 4) // 32-bit application
-            {
-                return new LibraryImport_x64();
-            }
-            else // 64-bit application
-            {
-                return null;
-            }
-        }
-    }
-
-    public interface ILibraryImport
-    {
-        point crs2crs_tranform(int source_cs, int target_cs, 
+        public LibraryImport() { }
+        [DllImport("proj_functions_x64", CallingConvention = CallingConvention.StdCall, ExactSpelling = false, EntryPoint = "crs2crs_tranform")]
+        private static extern point crs2crs_tranform_internal(int source_cs, int target_cs,
             double source_x, double source_y, double source_z);
 
-    }
-    internal class LibraryImport_x64 : ILibraryImport
-    {
-        [DllImport(@"C:\Users\Georg\Documents\GitHub\PROJ_NET_lib_cad\src\x64\Debug\proj_functions_x64",
-            CallingConvention = CallingConvention.StdCall, ExactSpelling = false, EntryPoint = "crs2crs_tranform")]
-        private static extern point crs2crs_tranform_internal(int source_cs, int target_cs, 
-            double source_x, double source_y, double source_z);
-
-        public point crs2crs_tranform(int source_cs, int target_cs, 
+        public point crs2crs_tranform(int source_cs, int target_cs,
             double source_x, double source_y, double source_z)
         {
             return crs2crs_tranform_internal(source_cs, target_cs, source_x, source_y, source_z);
         }
+        private bool disposed = false;
+
+        // реализация интерфейса IDisposable.
+        public void Dispose()
+        {
+            // освобождаем неуправляемые ресурсы
+            Dispose(true);
+            // подавляем финализацию
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing)
+            {
+                // Освобождаем управляемые ресурсы
+            }
+            // освобождаем неуправляемые объекты
+            disposed = true;
+        }
+
+        // Деструктор
+        ~LibraryImport()
+        {
+            Dispose(false);
+        }
+
     }
 }
